d5676a3bf6da667e244bde81fe416db67e7c3494
Aohk22 <96096597+Aohk22@users.noreply.github.com>
2025-11-21 19:15:48 +0700

    init

---
+++ lua/configs/options.lua
@@ -0,0 +1,15 @@
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+
+vim.opt.softtabstop = 0
+vim.opt.expandtab = false
+
+vim.opt.relativenumber = true
+vim.opt.number = true
+
+vim.opt.wrap = false
+
+vim.api.nvim_create_autocmd({'BufEnter'}, {
+	pattern = {'*.js', '*.css', '*.html', '*.lua'},
+	command = 'set shiftwidth=2 tabstop=2',
+})
