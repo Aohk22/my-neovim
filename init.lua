ade7a779332fdd708d14f803a32e1fb02b93451b
Aohk22 <96096597+Aohk22@users.noreply.github.com>
2025-11-21 22:18:07 +0700

    Add Clangd.

--- init.lua
+++ init.lua
@@ -3,3 +3,4 @@ require('packer')
 require('plugins') -- Load this after loading the package manager.
 
 vim.cmd('colorscheme kanagawa-dragon')
+vim.lsp.enable('clangd')
--- lua/packer.lua
+++ lua/packer.lua
@@ -7,6 +7,9 @@ vim.pack.add({
 		src = 'https://github.com/rebelot/kanagawa.nvim.git',
 		name = 'kanagawa',
 	},
+	{
+		src = 'https://github.com/neovim/nvim-lspconfig.git',
+	}
 })
 
 
