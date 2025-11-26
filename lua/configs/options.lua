79d4e356c3e2c13385e6436c3933b152cd2a9d43
Aohk22 <96096597+Aohk22@users.noreply.github.com>
2025-11-26 10:06:40 +0700

    Add keymap for showing diagnostics.

--- lua/configs/options.lua
+++ lua/configs/options.lua
@@ -9,15 +9,20 @@ vim.opt.number = true
 
 vim.opt.wrap = false
 
+vim.keymap.set("n", "gl", vim.diagnostic.open_float, 
+{
+	desc = "LSP: Show diagnostic float (current line)",
+})
+
 vim.api.nvim_create_autocmd({'BufEnter'}, 
 {
-	pattern = {'*.js', '*.css', '*.html', '*.lua'},
+	pattern = {'*.js', '*.c', '*.md', '*.css', '*.html', '*.cpp', '*.go', '*.yaml', '*.json', '*.lua'},
 	command = 'set shiftwidth=2 tabstop=2',
 })
 
 vim.api.nvim_create_autocmd({'FileType'},
 { 
-	pattern = {'*.js', '*.c', '*.md', '*.css', '*.html', '*.cpp'},
+	pattern = {'*.js', '*.c', '*.md', '*.css', '*.html', '*.cpp', '*.go', '*.yaml', '*.json'},
 	callback = function()
 		vim.treesitter.start()
 	end
