8e38e4229fb5aad35278e651a8f6c8c2c4a9b7e4
Aohk22 <96096597+Aohk22@users.noreply.github.com>
2026-02-28 21:54:04 +0700

    remove unsused configs

--- lua/configs/options.lua
+++ lua/configs/options.lua
@@ -1,34 +1,20 @@
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
-
 vim.opt.softtabstop = 0
 vim.opt.expandtab = false
 
 vim.opt.relativenumber = true
 vim.opt.number = true
-
 vim.opt.wrap = false
 
 vim.opt.timeoutlen = 2000
-
-vim.keymap.set('n', 'gl', vim.diagnostic.open_float,
-	{
-		desc = 'LSP: Show diagnostic float (current line)',
-	})
-
-vim.api.nvim_create_autocmd({ 'BufEnter' },
-	{
-		pattern = { '*.json' },
-		command = 'set shiftwidth=2 tabstop=2',
-	})
-
-vim.api.nvim_create_autocmd({ 'FileType' },
-	{
-		pattern = { '*.js', '*.c', '*.md', '*.css', '*.html', '*.cpp', '*.go', '*.yaml', '*.json', '*.jsx' },
-		callback = function()
-			vim.treesitter.start()
-		end
-	}
-)
-
 vim.opt.undofile = true
+
+vim.opt.laststatus = 3
+
+vim.keymap.set('n', 'gl', vim.diagnostic.open_float)
+
+vim.api.nvim_create_autocmd({ 'BufEnter' }, {
+	pattern = { '*.json' },
+	command = 'set shiftwidth=2 tabstop=2',
+})
