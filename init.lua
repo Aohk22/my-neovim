a0f5bd39fab283ac9cf0f1bc739b89276f82a907
Aohk22 <96096597+Aohk22@users.noreply.github.com>
2026-08-20 19:23:08 +0700

    fix: require order for some bug

--- init.lua
+++ init.lua
@@ -1,4 +1,4 @@
 require('packer')
-require('plugins') -- Load this after loading the package manager and before lsp.
 require('configs')
+require('plugins') -- Load this after loading the package manager and before lsp.
 require('lsp')
